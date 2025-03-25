6684de0ff3bed10ae00f53bd984d06483b6958fa
INub3 <michaelenriquez525@gmail.com>
2025-03-25 14:04:19 -0500

    :hammer: Colored v1.2

--- lat3.sh
+++ lat3.sh
@@ -1,44 +1,67 @@
 #!/bin/bash
 
+#Colours
+green="\e[0;32m\033[1m"
+end="\033[0m\e[0m"
+red="\e[0;31m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+purple="\e[0;35m\033[1m"
+gray="\e[0;37m\033[1m"
+turq="\e[0;36m\033[1m"
+
 trap ctrl_c INT
 
 function ctrl_c(){
-  echo "\n[!] Proceso detenido..\n"
+  echo -e "\n${red}[!] Proceso detenido..${end}\n"
   exit 1
 }
 
 function helpPanel(){
-  echo -e "[+] Uso: ./lat3.sh \n\n"
-  echo -e "[*] Parametros:\n"
-  echo -e "\t -l  Lenguaje de salida | Ejemplo: -l en -> Lenguajes disponibles trans -R" 
-  echo -e "\t -t  Establece el tiempo de escucha en segundos -> -t 10"
-  echo -e "\t -f  Traduce un archivo"
-  echo -e "\t -m  Modo de Traducción -> Por entrada de voz (listen)"
-  echo -e "\t -h  Panel de Ayuda"
+  echo -e "
+  ╭╮${yellow}╱╱${end}${blue}╱╱${end}${red}╱${end}╭╮╭━━━╮
+  ┃┃${yellow}╱${end}${blue}╱╱${end}${red}╱${end}╭╯╰┫╭━╮┃
+  ┃┃${blue}╱╱${end}╭━┻╮╭┻╯╭╯┃
+  ┃┃${blue}╱${end}╭┫╭╮┃┃╭╮╰╮┃
+  ┃╰━╯┃╭╮┃╰┫╰━╯┃
+  ╰━━━┻╯╰┻━┻━━━╯"
+  echo -e "\n${blue}[+] Uso:${end} ./lat3.sh \n\n"
+  echo -e "${purple}[*] Parametros:${end}\n"
+  echo -e "\t ${green}-l${end}  Lenguaje de salida | Ejemplo: -l en"
+  echo -e "\t     Lenguajes disponibles trans -R"
+  echo -e "\t ${green}-t${end}  Tiempo de escucha (seg) | -t 10"
+  echo -e "\t ${green}-f${end}  Traduce un archivo"
+  echo -e "\t ${green}-m${end}  Modo de Traducción -> Por entrada de voz (listen)"
+  echo -e "\t ${green}-h${end}  Panel de Ayuda"
 
-  echo -e "\n[*] Ejemplo de ejecución: ./lat3.sh -m listen -l en -t 10"
+  echo -e "\n${purple}[*] Ejemplo de ejecución:${end} ${gray}./lat3.sh -m listen -l en -t 10${end}"
 }
 
 function listen_mode(){
-  echo -e "\n[*] Escuchando... ($listen_time segs)"
-  arecord -f cd -d $listen_time input.wav > /dev/null 2>&1
-  sleep 1
+  if [ ! -d "history" ]; then
+    mkdir history
+  fi
 
-  echo -e "\n[+] Convirtiendo Voz a Texto..."
+  echo -e "\n${purple}[*] Escuchando...${end} ${gray}($listen_time segs)${end}"
+  arecord -f cd -d $listen_time input.wav > /dev/null 2>&1
+
+  echo -e "\n${turq}[+] Convirtiendo Voz a Texto...${end}"
   ffmpeg -i input.wav -ar 16000 -ac 1 -f wav audio.wav >/dev/null 2>&1
 
   python3 cribe.py audio.wav > result.txt 2>&1
 
   tail -n 1 result.txt > output.txt
-  echo -e "\nHas dicho:"
+  echo -e "\n${yellow}[-] Has dicho:${end}"
   cat output.txt
 
-  traduccion=$(trans -b es:$lenguaje -i output.txt -o ./history/traduccion_$(date | cut -d " " -f5).txt)
-  echo -e "\nTexto Traducido:"
+  traduccion=$(trans -b es:$lenguaje -i output.txt)
+  echo -e "\n${yellow}[-] Texto Traducido:${end}"
   echo $traduccion
 
+  # Gestion de archivos e historial
+  echo $traduccion > ./history/traduccion_$(date | cut -d " " -f5).txt
   mv audio.wav ./history/audio_$(date | cut -d " " -f5).wav
-  mv output.txt ./history/output$(date | cut -d " " -f5).txt
+  mv output.txt ./history/output_$(date | cut -d " " -f5).txt
   rm input.wav result.txt > /dev/null 2>&1
 }
 
@@ -60,7 +83,7 @@ else
   if [ $mode = "listen" ]; then
     listen_mode
   else
-    echo "[!] El modo indicado no existe"
+    echo "${red}[!] El modo indicado no existe${end}"
     helpPanel
   fi
 fi
